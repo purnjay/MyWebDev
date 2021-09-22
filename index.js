332f14055322ddd619493728152fa063bbc855de
Purnjay <purnjay19@gmail.com>
2021-09-22 05:49:09 -0400

    7th Commit
     + Working Time command,
        Type "Time" in the textbox to get the
         current local time.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-var input_bar = document.getElementById("main-srch-bar");
+const input_bar = document.getElementById("main-srch-bar").value;
 var output = document.getElementById("placeholder");
 
+output.addEventListener("animationend", () => {
+    output.style.display = "none";
+})
+
+document.addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        event.preventDefault();
+        time();
+    }
+    });
+
 function time(){
-   
+    if(document.getElementById("main-srch-bar").value == "Time"){
+        var dt = new Date();
+        output.innerHTML = dt.toLocaleTimeString();
+        output.style.display = "block";
+    }
 }
