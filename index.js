fd5dc3b0139f3c0dcde85546fe08c71be51c6f4a
Purnjay <purnjay19@gmail.com>
2021-09-22 08:51:34 -0400

    9th Commit
    Can select colors now
    
    -Green
    -Grey

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const input_bar = document.getElementById("main-srch-bar").value;
+
 var output = document.getElementById("placeholder");
+var settings = document.getElementById("settings");
 
 output.addEventListener("animationend", () => {
     output.style.display = "none";
@@ -8,14 +10,26 @@ output.addEventListener("animationend", () => {
 document.addEventListener("keyup", function(event) {
     if (event.key == "Enter") {
         event.preventDefault();
-        time();
+        Execute();
     }
     });
 
-function time(){
+function Execute(){
+    
     if(document.getElementById("main-srch-bar").value == "Time"){
         var dt = new Date();
         output.innerHTML = dt.toLocaleTimeString();
         output.style.display = "block";
     }
+
+    if(document.getElementById("main-srch-bar").value == "Color"){
+        settings.style.display = "block";
+    }
+
+}
+
+function colorSelected(colorname){
+    document.getElementById("main-srch-bar").style.color = colorname;
+    output.style.color = colorname;
+    settings.style.display = "none";
 }
